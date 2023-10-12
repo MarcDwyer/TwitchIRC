79fe1b7eddd9d669fd8be08758279f863d7d4c1d
Marc Dwyer <marcdwyer1738@gmail.com>
2023-10-11 21:20:04 -0500

    removed logic from render function

--- src/routes/Trollerino/components/IRCView/JoinedTabs.tsx
+++ src/routes/Trollerino/components/IRCView/JoinedTabs.tsx
@@ -1,18 +1,12 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faX } from "@fortawesome/free-solid-svg-icons";
-import { useRecoilState, useRecoilValue } from "recoil";
-import { joinedState } from "@src/routes/Trollerino/atoms/joined";
-import { messagesState } from "@src/routes/Trollerino/atoms/messages";
 import { useMemo } from "react";
 import { useActiveChannel } from "../../hooks/useActiveChannel";
-import { ircSocketState } from "../../atoms/ircSocket";
+import { useJoined } from "../../hooks/useJoined";
 
 export const JoinedTabs = () => {
-  const [joined, setJoined] = useRecoilState(joinedState);
   const { activeChannel, setActiveChannelName } = useActiveChannel();
-  const [, setMessages] = useRecoilState(messagesState);
-  const ws = useRecoilValue(ircSocketState);
-
+  const { joined, part } = useJoined();
   const streams = useMemo(() => Array.from(joined.values()), [joined]);
 
   return (
@@ -35,20 +29,7 @@ export const JoinedTabs = () => {
               <div className="flex flex-col">
                 <div
                   onClick={() => {
-                    // part
-                    if (ws) {
-                      ws.send(`PART ${stream.channelName}`);
-                    }
-                    setJoined((currJoined) => {
-                      const updatedJoined = new Map(currJoined);
-                      updatedJoined.delete(stream.channelName);
-                      return updatedJoined;
-                    });
-                    setMessages((currMessages) => {
-                      const updatedMsgs = new Map(currMessages);
-                      updatedMsgs.delete(stream.channelName);
-                      return updatedMsgs;
-                    });
+                    part(stream.channelName);
                   }}
                   className="ml-auto"
                 >
