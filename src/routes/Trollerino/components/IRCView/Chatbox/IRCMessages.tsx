2dc70e428502e7273d51a0a7be4473bd3bf65e07
Marc Dwyer <marcdwyer1738@gmail.com>
2023-10-20 15:21:21 -0500

    moved chat scroll logic to hook

--- src/routes/Trollerino/components/IRCView/Chatbox/IRCMessages.tsx
+++ src/routes/Trollerino/components/IRCView/Chatbox/IRCMessages.tsx
@@ -1,5 +1,6 @@
 import { JoinedAtomValue } from "@src/routes/Trollerino/atoms/joined";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
+import { useChatPause } from "./hooks/useChatPause";
 
 type Props = {
   activeChannel: JoinedAtomValue;
@@ -8,66 +9,22 @@ type Props = {
 };
 // may need to split this up into multiple custom hooks
 export function IRCMessages({ activeChannel, pause, unpause }: Props) {
-  const parentRef = useRef<any>();
-  const prevPause = useRef(activeChannel.paused);
-  const setBottom = () => {
-    parentRef.current.scrollTop = parentRef.current.scrollHeight;
-  };
-  const checkIfBottom = () => {
-    const { scrollTop, scrollHeight, clientHeight } = parentRef.current;
-    return scrollHeight - scrollTop === clientHeight;
-  };
-  useEffect(() => {
-    if (parentRef.current && !activeChannel.paused) {
-      setBottom();
-    }
-  }, [activeChannel.messages, parentRef, activeChannel.paused]);
+  const { chatEleRef, setChatToBottom } = useChatPause({
+    pause,
+    unpause,
+    paused: activeChannel.paused,
+  });
 
   useEffect(() => {
-    if (prevPause.current !== activeChannel.paused) {
-      setBottom();
+    if (!activeChannel.paused) {
+      setChatToBottom();
     }
-    prevPause.current = activeChannel.paused;
-  }, [activeChannel.paused]);
-
-  useEffect(() => {
-    let interval: number;
-
-    if (activeChannel.paused) {
-      interval = setInterval(() => {
-        const isBottom = checkIfBottom();
-        console.log({ isBottom });
-        if (isBottom) {
-          unpause();
-          clearInterval(interval);
-        }
-      }, 350);
-    }
-
-    return function () {
-      if (interval) {
-        clearInterval(interval);
-      }
-    };
-  }, [activeChannel.paused, unpause, parentRef.current]);
+  }, [activeChannel.paused, activeChannel.messages]);
 
   return (
     <div
-      onWheel={() => {
-        pause();
-        // const { scrollTop, scrollHeight, clientHeight } = parentRef.current;
-        // const height = scrollTop + clientHeight;
-        // // const percentageHeight = scrollHeight * 0.05;
-        // const isBottom = height >= scrollHeight - scrollHeight;
-        // console.log(activeChannel.paused, isBottom);
-
-        // if (activeChannel.paused && isBottom) {
-        //   setPaused(false);
-        // } else {
-        //   setPaused(true);
-        // }
-      }}
-      ref={parentRef}
+      onWheel={pause}
+      ref={chatEleRef}
       className="overflow-y-auto m-auto w-11/12 overflow-x-hidden mt-5 mb-5 h-full"
     >
       {activeChannel.paused && (
